77c828032c630f0e856ba6c0f6d9551c4bda89fc
kusogray <kusogray1@gmail.com>
2016-03-07 19:21:22 +0800

    002_sample
    test performance

--- BNP/Curiosity/002_sample_20.py
+++ BNP/Curiosity/002_sample_20.py
@@ -17,32 +17,40 @@ Feb 1, 2016     1.0.0     Init.
 
 
     
-import pandas as pd
-import numpy as np
-import csv
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn import ensemble
-from BNP.Resources import Config
-from BNP.util.CustomLogger import musicAlarm
 from BNP.util.CustomLogger import info as log
+from BNP.DataCollector.DataStratifier import stratifyData
+from BNP.Resources import Config
+from BNP.DataCollector.DataReader import DataReader as DataReader
 from BNP.DataAnalyzer.ModelFactory import ModelFactory as ModelFactory
+from BNP.util.CustomLogger import musicAlarm
+from BNP.util.ModelUtils import loadModel
+from BNP.util.ModelUtils import getMatchNameModelPath
+from BNP.util.ModelUtils import deleteModelFiles
+import pandas as pd
+from BNP.Bartender.Blender import Blender
+from test._mock_backport import inplace
+import random
 
 if __name__ == '__main__':
     
     log('Load data...')
-    expInfo = "001_baseline"
+    expInfo = "002_sample"
     _basePath = Config.FolderBasePath + expInfo + Config.osSep
     trainPath = _basePath + "train.csv"
     testPath = _basePath + "test.csv"
     outputPath = _basePath + "ans.csv"
     
-    train = pd.read_csv(trainPath)
-    target = train['target'].values
-    train = train.drop(['ID','target'],axis=1)
-    test = pd.read_csv(testPath)
-    id_test = test['ID'].values
-    test = test.drop(['ID'],axis=1)
+    dr = DataReader()
+    dr.readInCSV(trainPath, "train")
+    dr.readInCSV(testPath, "test")
+    dr.doSample(0.2)
+    
+    
+    train = dr._trainSampleDf
+    target = dr._ansSampleDf
+    test = dr._testDataFrame
+    id_test = dr._testIdDf
+    
     
     log('Clearing...')
     for (train_name, train_series), (test_name, test_series) in zip(train.iteritems(),test.iteritems()):
@@ -67,26 +75,18 @@ if __name__ == '__main__':
     X_train = train
     X_test = test
     
-    train.to_csv(_basePath+"train_after",index=False)
-    exit()
     
     fab = ModelFactory()
     fab._gridSearchFlag = True
     fab._singleModelMail = True
-    fab._subFolderName = "baseline"  
-    fab._n_iter_search = 3
+    fab._subFolderName = expInfo
+    fab._n_iter_search = 10
     fab._expInfo = expInfo
-    extc = fab.getExtraTressClf(X_train, target)
-    
-#     extc = ExtraTreesClassifier(n_estimators=700,max_features= 50,criterion= 'entropy',min_samples_split= 5,
-#                                 max_depth= 50, min_samples_leaf= 5)      
-#     
-#     extc.fit(X_train,target) 
+    clf = fab.getExtraTressClf(train, target)
     
     log('Predict...')
-    y_pred = extc.predict_proba(X_test)
-    #print y_pred
+    y_pred = clf.predict_proba(X_test)
     
-    pd.DataFrame({"ID": id_test, "PredictedProb": y_pred[:,1]}).to_csv('extra_trees.csv',index=False)
+    pd.DataFrame({"ID": id_test, "PredictedProb": y_pred[:,1]}).to_csv(outputPath,index=False)
     musicAlarm()
         
--- BNP/DataCollector/DataReader.py
+++ BNP/DataCollector/DataReader.py
@@ -5,6 +5,7 @@ Created on Jan 24, 2016
 '''
 
 import pandas as pd
+import numpy as np
 from BNP.util.CustomLogger import info as log
 
 class DataReader(object):
@@ -13,8 +14,10 @@ class DataReader(object):
     '''
     _trainDataFrame, _testDataFrame, _ansDataFrame = [],[],[]
     
-    _trainSampleDf, _trainEvalSampleDf = [], []
-    _ansSampleDf, _ansEvalSampleDf = [], []
+    _trainSampleDf, _trainRestDf = [], []
+    _ansSampleDf, _ansRestDf = [], []
+    
+    _testIdDf = []
 
     def __init__(self, ):
         '''
@@ -25,16 +28,24 @@ class DataReader(object):
         df = pd.read_csv(path, header=0, sep=',')
         log("loading csv: " + path)
         if mode.lower() == "train":
-            self._ansDataFrame = df[df.columns[0]]
-            self._trainDataFrame = df[df.columns[1:]]
+            self._ansDataFrame = df[df.columns[1]]
+            self._trainDataFrame = df[df.columns[2:]]
         else:
-            self._testDataFrame = df
+            self._testIdDf = df[df.columns[0]]
+            self._testDataFrame = df[df.columns[1:]]
     
-    def cvtPathListToDfList(self, inputPath, mode):
-        self.readInCSV(inputPath, mode)
-        if mode.lower() == "train":
-            return self._trainDataFrame, self._ansDataFrame
-        else:
-            return self._testDataFrame
+    
+    
+    def doSample(self, samplePercent):
         
+        X = self._trainDataFrame
+        Y = self._ansDataFrame
+        sampleRows = np.random.choice(X.index, len(X)*samplePercent) 
+                
+        
+        self._trainSampleDf  =  X.ix[sampleRows]
+        self._ansSampleDf = Y.ix[sampleRows]
+        
+        self._trainRestDf  =  X.drop(sampleRows)
+        self._ansRestDf = Y.drop(sampleRows)
         
