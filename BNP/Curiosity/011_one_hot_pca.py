7ff5d57a233fe4307b8d852a8503ff5c75262372
whmou <whmou0@gmaill.com>
2016-10-29 20:03:13 +0800

    Test for change

--- BNP/Curiosity/011_one_hot_pca.py
+++ BNP/Curiosity/011_one_hot_pca.py
@@ -6,17 +6,6 @@ Feb 1, 2016     1.0.0     Init.
 
 '''
 
-# from BNP.util.CustomLogger import info as log
-# from BNP.DataCollector.DataStratifier import stratifyData
-# from BNP.Resources import Config
-# from BNP.DataCollector.DataReader import DataReader as DataReader
-# from BNP.DataAnalyzer.ModelFactory import ModelFactory as ModelFactory
-# from BNP.util.CustomLogger import musicAlarm
-# from BNP.util.ModelUtils import loadModel
-# import pandas as pd
-
-
-    
 from BNP.util.CustomLogger import info as log
 from BNP.DataCollector.DataStratifier import stratifyData
 from BNP.Resources import Config
